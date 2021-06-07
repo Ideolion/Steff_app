9a7d58fd3486c57ce80bef5edd153afe7bfe6d32
Ideolion <skegul24@gmail.com>
2021-06-07 16:53:32 +0300

    v2

--- app/src/main/java/com/example/ashram_app/AddYoutubeVideo/AddYoutubeVideo.java
+++ app/src/main/java/com/example/ashram_app/AddYoutubeVideo/AddYoutubeVideo.java
@@ -4,13 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ashram_app.AddVideo.AddVideoActivity;
 import com.example.ashram_app.MainActivity;
 import com.example.ashram_app.R;
+import com.example.ashram_app.ui.home.DataSetList;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class AddYoutubeVideo extends AppCompatActivity {
     EditText YoutubeURL;
@@ -36,10 +43,24 @@ public class AddYoutubeVideo extends AppCompatActivity {
     public void UploadLink(View view) {
         YoutubeURL = findViewById(R.id.YoutubeURL);
         URL = YoutubeURL.getText().toString();
+        DataSetList dataSetList = new DataSetList(URL);
 
-
-       // https://youtu.be/mdd27VwuWQ0
         FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("YoutubeLink")
+                .add(dataSetList)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Ссылка загружена", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Ошибка загрузки", Toast.LENGTH_SHORT).show();
+                    }
+                });
 
 
 
