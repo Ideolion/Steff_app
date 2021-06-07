167dbcd1837b5c44989ae20e104ca0c952abdfaf
Ideolion <skegul24@gmail.com>
2021-06-07 17:33:38 +0300

    Добавил фрагменты и избранное

--- app/src/main/java/com/example/ashram_app/MainActivity.java
+++ app/src/main/java/com/example/ashram_app/MainActivity.java
@@ -47,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_meditation)
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_meditation, R.id.favoriteVideo, R.id.favoriteVideo)
                 .setDrawerLayout(drawer)
                 .build();
 
